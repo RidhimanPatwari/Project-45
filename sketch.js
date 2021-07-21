f28276aee500670ba5c9154718685602a2524545
RidhimanPatwari <65568269+RidhimanPatwari@users.noreply.github.com>
2021-07-21 12:08:16 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var playButtonImage;
+var homepage, homepageImage;
+var playButton = document.getElementById("playButton");
+var gameState = "homepage";
+var image;
+var button;
+
+function preload() {
+  playButtonImage = loadImage("images/play_button.png");
+  homepageImage = loadImage("images/homepage.png");
+}
+
+function setup() {
+  createCanvas(1200,800);
+}
+
+function draw() {
+  background(255,255,255); 
+
+  if (gameState === "homepage") {
+    playButton.style.opacity = 0;
+    homepage = createSprite(600, 400, 1200, 800);
+    homepage.addImage(homepageImage);
+    drawSprites();
+
+    playButton.onclick = function() {
+      playButton.remove();
+      gameState = "play";
+    }
+
+  }
+
+  if (gameState === "play") {
+    textSize(30);
+    text("Work In Progress", 200, 200);
+  }
+
+}
